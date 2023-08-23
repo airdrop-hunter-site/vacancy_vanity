6cc99e1d320c3faf9d2dfb61f30b3b793fc6dc93
VitaliyTarasov <vivasaravian@gmail.com>
2023-08-23 11:30:29 +0300

    fix template view

--- htdocs/inc/inc/beta.php
+++ htdocs/inc/inc/beta.php
@@ -1,3 +1,3 @@
 <?php
-print "<div class=\"top-info\" id=\"top-info-id\">This project is beta. DYOR </div>";
+// print "<div class=\"top-info\" id=\"top-info-id\">This project is beta. DYOR </div>";
 ?>
